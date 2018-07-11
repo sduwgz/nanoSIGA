af5555a3aee98ef371859aff32964d4fc714d1d7
weiguozheng@ict.ac.cn <weiguozheng@ict.ac.cn>
2018-07-11 22:53:34 +0800

    :sparkles: add alignmentreader

--- src/mole.cpp
+++ src/mole.cpp
@@ -32,8 +32,8 @@ bool MoleReader::read(Mole& mole) {
     enum {
         moleId,
         molePosition,
-        moleQX01,
-        moleQX02,
+        moleQX11,
+        moleQX12,
     };
     reset(mole);
     int state = moleId;
@@ -62,17 +62,97 @@ bool MoleReader::read(Mole& mole) {
                 if (mole._position.size() > 1) {
                     mole.getDistance();
                 }
-                state = moleQX01;
+                state = moleQX11;
             } else {
                 LOG4CXX_WARN(logger, boost::format("Load molecule file failed(invalid line for mole position: %s)") % buf);
                 return false;
             }
-        } else if (state == moleQX01) {
-            state = moleQX02;
-        } else if (state == moleQX02) {
+        } else if (state == moleQX11) {
+            data = boost::algorithm::split(data, buf, boost::algorithm::is_any_of(" \t"), boost::algorithm::token_compress_on);
+            if (data[0] == "QX11") {
+                std::vector<double> qx11;
+                for (int i = 1; i < data.size(); ++ i) {
+                    qx11.push_back(static_cast< long > (boost::lexical_cast< double >(data[i])));
+                }
+                int l = 0;
+                //theta is a threshold for SNR
+                double theta = 3;
+                //filter sites by QX11 SNR
+                if(qx11.size() == mole._position.size()) {
+                    for(int i = 0; i < mole._position.size(); ++ i) {
+                        if(qx11[i] >= theta) {
+                            mole._position[l ++] = mole._position[i];
+                        }
+                    }
+                    mole._position.resize(l);
+                }
+                if (mole._position.size() > 1) {
+                    mole.getDistance();
+                }
+                state = moleQX12;
+            } else {
+                LOG4CXX_WARN(logger, boost::format("bnx=>invalid line for mole QX11: %s") % buf);
+                return false;
+            }
+            state = moleQX12;
+        } else if (state == moleQX12) {
             state = moleId;
             return true;
         }
+    }
+    return false;
+}
+bool AlignmentReader::read(Alignment& al) {
+    if(!_stream){
+        return false;
+    }
+    std::string buf;
+    std::vector<std::string> data;
+    if(std::getline(_stream, buf)) {
+        boost::algorithm::trim(buf);
+        LOG4CXX_DEBUG(logger, boost::format("line: %s") % buf);
+
+        boost::algorithm::split(data, buf, boost::algorithm::is_any_of(" \t"), boost::algorithm::token_compress_on);
+        al.mole1Id = data[0];
+        al.mole2Id = data[1];
+        al.score = boost::lexical_cast< double >(data[2]);
+
+        al.mole1Start = boost::lexical_cast< int >(data[3]);
+        al.mole2Start = boost::lexical_cast< int >(data[4]);
+        al.mole1End = boost::lexical_cast< int >(data[5]);
+        al.mole2End = boost::lexical_cast< int >(data[6]);
+
+        std::getline(_stream, buf);
+        boost::algorithm::trim(buf);
+        LOG4CXX_DEBUG(logger, boost::format("line: %s") % buf);
+        boost::algorithm::split(data, buf, boost::algorithm::is_any_of(" \t"), boost::algorithm::token_compress_on);
+        AlignedMole a1, a2;
+        for(auto fragstr : data) {
+            Fragment frag;
+            std::vector<std::string> temp;
+            boost::algorithm::split(temp, fragstr, boost::algorithm::is_any_of(","), boost::algorithm::token_compress_on);
+            
+            for(std::string intervalstr : temp) {
+                frag.push_back(boost::lexical_cast< int >(intervalstr));
+            }
+            a1.push_back(frag);
+        }
+        std::getline(_stream, buf);
+        boost::algorithm::trim(buf);
+        LOG4CXX_DEBUG(logger, boost::format("line: %s") % buf);
+        boost::algorithm::split(data, buf, boost::algorithm::is_any_of(" \t"), boost::algorithm::token_compress_on);
+        for(auto fragstr : data) {
+            Fragment frag;
+            std::vector<std::string> temp;
+            boost::algorithm::split(temp, fragstr, boost::algorithm::is_any_of(","), boost::algorithm::token_compress_on);
+            for(std::string intervalstr : temp) {
+                frag.push_back(boost::lexical_cast< int >(intervalstr));
+            }
+            a2.push_back(frag);
+        }
+        al.alignedMole1 = a1;
+        al.alignedMole2 = a2;
+        return true;
     }
     return false;
 }
