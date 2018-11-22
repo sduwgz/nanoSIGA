e4e339f5cb20890c46fe4ae7ddb35da5c9e264d2
weiguozheng@ict.ac.cn <weiguozheng@ict.ac.cn>
2018-11-22 15:11:44 +0800

    :sparkles: add two parameters(reverseLabel, useHash)

--- src/overlap.cpp
+++ src/overlap.cpp
@@ -18,7 +18,7 @@ class Overlapping : public Runner {
 public:
     virtual int run(const Properties options, const Arguments& arg);
 private:
-    Overlapping() : Runner("m:p:o:t:", boost::assign::map_list_of('m',"minscore")('p', "parameter")('o', "prefix")('t', "threads")) {
+    Overlapping() : Runner("m:p:o:t:SR", boost::assign::map_list_of('m',"minscore")('p', "parameter")('o', "prefix")('t', "threads")('S', "hash")('R', "reverse")) {
         RunnerManager::instance()->install("overlap", this);
     }
     int checkOptions(const Properties options, const Arguments& args) const {
@@ -37,6 +37,8 @@ private:
                 "\n"
                 "      -t, --threads=NUM                use NUM threads to construct the index (default: 1)\n"
                 "      -m, --min-score=f                minimum aligment score required between two reads (default: 5)\n"
+                "      -S, --hash                       use hash strategy to speed up\n"
+                "      -R, --reverse                    reverse mole as a new mole\n"
                 "      -o, --prefix=PREFIX              write index to file using PREFIX instead of prefix of READSFILE\n"
                 "\n"
                 ) << std::endl;
@@ -65,8 +67,14 @@ int Overlapping::run(const Properties options, const Arguments& arg) {
     }
     LOG4CXX_INFO(logger, boost::format("output file is: %s") % output);
 
+    bool useHash = false, useReverse = false;
+    if(options.find("hash") != options.not_found()) {
+        useHash = true;
+        LOG4CXX_INFO(logger, boost::format("hash strategy has been used to speed up."));
+    }
+    if(options.find("reverse") != options.not_found()) useReverse = true;
     OverlapBuilder builder(parameter_file, output);
-    if(!builder.build(input, options.get< double >("minscore", 15.0), output, options.get< size_t >("threads", 1))) {
+    if(!builder.build(input, options.get< double >("minscore", 15.0), output, options.get< size_t >("threads", 1), 1, useReverse, useHash)) {
         LOG4CXX_ERROR(logger, boost::format("Failed to build overlap from %s MOLECULEs") % input);
         r = -1;
     }
