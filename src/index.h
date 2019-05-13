ec6b2e253da575a83f7a43c64e62c134d09d2692
weiguozheng@ict.ac.cn <weiguozheng@ict.ac.cn>
2019-05-13 21:07:24 +0800

    add a default constructer in index.h.

--- src/index.h
+++ src/index.h
@@ -9,6 +9,7 @@ typedef std::pair<std::string, int> Value;
 typedef std::unordered_map<std::string, std::set<Value>> HashTable;
 class Index {
 public:
+    Index() {}
     Index(int k) : K(k) {}
     void build(const std::vector<Mole>& moleSet);
     std::set<std::string> query(const Mole& m) const;
